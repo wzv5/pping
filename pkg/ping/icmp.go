fa81d3bd34ea0b290561de4ae71a33313d2395b3
wzv5 <wzv5@msn.com>
2020-07-29 23:51:32 +0800

    清理

--- pkg/ping/icmp.go
+++ pkg/ping/icmp.go
@@ -3,7 +3,6 @@ package ping
 import (
 	"bytes"
 	"context"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"math/rand"
@@ -292,33 +291,6 @@ func (this *IcmpPing) errorResult(err error) IPingResult {
 	r := &IcmpPingResult{}
 	r.Err = err
 	return r
-}
-
-func bytesToTime(b []byte) time.Time {
-	var nsec int64
-	for i := uint8(0); i < 8; i++ {
-		nsec += int64(b[i]) << ((7 - i) * 8)
-	}
-	return time.Unix(nsec/1000000000, nsec%1000000000)
-}
-
-func timeToBytes(t time.Time) []byte {
-	nsec := t.UnixNano()
-	b := make([]byte, 8)
-	for i := uint8(0); i < 8; i++ {
-		b[i] = byte((nsec >> ((7 - i) * 8)) & 0xff)
-	}
-	return b
-}
-
-func bytesToInt(b []byte) int64 {
-	return int64(binary.BigEndian.Uint64(b))
-}
-
-func intToBytes(tracker int64) []byte {
-	b := make([]byte, 8)
-	binary.BigEndian.PutUint64(b, uint64(tracker))
-	return b
 }
 
 var (
